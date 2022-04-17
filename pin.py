46276241c7af9e3612f825949938b52ba9a182e0
Kr1shwest <102893961+Kr1shwest@users.noreply.github.com>
2022-04-17 10:26:42 +0400

    v3 добавил GameSprite для 2 платформ и 2 класса для спрайтов

--- pin.py
+++ pin.py
@@ -4,10 +4,10 @@ win = display.set_mode((1280, 720))
 display.set_caption("Ping-pong")
 back = transform.scale(image.load("back.jpg"), (1280, 720))
 
-class GameSprite(sprite.Sprite):
+class GameSpriteL(sprite.Sprite):
     def __init__(self, player_image, player_x, player_y, size_x, size_y, player_speed):
         super().__init__()
-        self.image = transform.scale(image.load(player_image), (size_x, size_y))    
+        self.image = transform.scale(image.load(left), (size_x, size_y))    
         self.speed = player_speed
         self.rect = self.image.get_rect()
         self.rect.x = player_x
@@ -16,6 +16,33 @@ class GameSprite(sprite.Sprite):
     def reset(self):
         win.blit(self.image, (self.rect.x, self.rect.y))
 
+class GameSpriteR(sprite.Sprite):
+    def __init__(self, player_image, player_x, player_y, size_x, size_y, player_speed):
+        super().__init__()
+        self.image = transform.scale(image.load(right), (size_x, size_y))    
+        self.speed = player_speed
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y 
+        self.direction = "left"
+    def reset(self):
+        win.blit(self.image, (self.rect.x, self.rect.y))
+
+class Left(GameSprite):
+    def update(self):
+        keys = key.get_pressed()
+        if keys[K_UP] and self.rect.x > 0:
+            self.rect.x -= self.speed
+        if keys[K_DOWN] and self.rect.x < 650:
+            self.rect.x += self.speed
+
+class Right(GameSprite):
+    def update(self):
+        keys = key.get_pressed()
+        if keys[K_UP] and self.rect.x > 0:
+            self.rect.x -= self.speed
+        if keys[K_DOWN] and self.rect.x < 650:
+            self.rect.x += self.speed
 
 clock = time.Clock()
 
