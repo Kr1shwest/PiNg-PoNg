1762f0ab305437f16c1bddec3c6681d1a884d7d6
Kr1shwest <102893961+Kr1shwest@users.noreply.github.com>
2022-04-17 10:18:10 +0400

    v2 Добавил класс GameSprite

--- pin.py
+++ pin.py
@@ -4,6 +4,19 @@ win = display.set_mode((1280, 720))
 display.set_caption("Ping-pong")
 back = transform.scale(image.load("back.jpg"), (1280, 720))
 
+class GameSprite(sprite.Sprite):
+    def __init__(self, player_image, player_x, player_y, size_x, size_y, player_speed):
+        super().__init__()
+        self.image = transform.scale(image.load(player_image), (size_x, size_y))    
+        self.speed = player_speed
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y 
+        self.direction = "left"
+    def reset(self):
+        win.blit(self.image, (self.rect.x, self.rect.y))
+
+
 clock = time.Clock()
 
 game = True
